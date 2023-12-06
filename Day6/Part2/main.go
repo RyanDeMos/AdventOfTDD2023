2a2f453c526b5af6a4a50e03811bc0541dd2ca35
Ryan de Mos <ryanjdemos@gmail.com>
2023-12-06 18:19:05 -0500

    Optimized for faster run time.

--- Day6/Part2/main.go
+++ Day6/Part2/main.go
@@ -13,7 +13,6 @@ import (
 
 func main() {
 	start := time.Now()
-
 	// file, err := os.Open("./Day6/Part1/input/testInput.txt")
 	file, err := os.Open("./Day6/Part1/input/InputFile.txt")
 	if err != nil {
@@ -31,6 +30,7 @@ func main() {
 
 	raceDurations := parseLine(fileLines[0])
 	raceDistances := parseLine(fileLines[1])
+
 	amountOfSolutions := getPossibleDistances(raceDistances, raceDurations)
 
 	fmt.Printf("Amount of solutions is %v\n", amountOfSolutions)
@@ -40,13 +40,7 @@ func main() {
 }
 
 func parseLine(line string) int {
-	firstArray := strings.Split(line[9:], " ")
-	combinedDigits := ""
-	for _, word := range firstArray {
-		if word != "" {
-			combinedDigits += word
-		}
-	}
+	combinedDigits := strings.ReplaceAll(line[9:], " ", "")
 	singleInt, err := strconv.Atoi(combinedDigits)
 	if err != nil {
 		log.Fatal(err)
@@ -54,16 +48,13 @@ func parseLine(line string) int {
 	return singleInt
 }
 
-func getPossibleDistances(distance int, time int) int {
-	fmt.Printf("Time is: %d\n", time)
-	fmt.Printf("DIstance is: %d\n", distance)
-	lowerRoot := ((float64(time)) - math.Sqrt(math.Pow(float64(time), 2)-4*float64(distance))) / (2)
-	upperRoot := ((float64(time)) + math.Sqrt(math.Pow(float64(time), 2)-4*float64(distance))) / (2)
-	fmt.Printf("Lower bound :%v\n", lowerRoot)
-	fmt.Printf("Upper bound :%v\n", upperRoot)
-	amountOfSolutions := 0
-	for j := int(lowerRoot) + 1; float64(j) < upperRoot; j++ {
-		amountOfSolutions += 1
+func getPossibleDistances(distance int, raceDuration int) int {
+	lowerRoot := ((float64(raceDuration)) - math.Sqrt(math.Pow(float64(raceDuration), 2)-4*float64(distance))) / (2)
+	upperRoot := ((float64(raceDuration)) + math.Sqrt(math.Pow(float64(raceDuration), 2)-4*float64(distance))) / (2)
+	// If upperRoot is an int (unlikely) then the amount of integers between the two roots is different
+	if upperRoot != float64(int(upperRoot)) {
+		return int(upperRoot) - int(lowerRoot)
+	} else {
+		return int(upperRoot) - int(lowerRoot) - 1
 	}
-	return amountOfSolutions
 }
